3dfa91409bcf3530fcdeb8c46bff754e39e8304b
cameronmcewan <cm17521@bristol.ac.uk>
2022-09-20 17:03:00 +0100

    create-react-app

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { MoralisProvider } from "react-moralis";
+import { BrowserRouter } from "react-router-dom";
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId="wfaERX2SQjRaJsW0GEwdILsuDGt31CGjuJzBsiv9" serverUrl="https://shsgffygizec.usemoralis.com:2053/server">
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
